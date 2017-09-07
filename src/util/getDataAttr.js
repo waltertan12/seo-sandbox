f35cc458c20258b0159288e530ed125c352b96a9
Walter Tan <wtan@coursehero.com>
2017-09-07 09:36:14 -0700

    Updates

--- src/util/getDataAttr.js
+++
@@ -1,9 +0,0 @@
-/**
- * getDataAttr.js
- *
- * Gets a data attribute from a provided DOM element (like jQuery's $.data).
- *
- * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
- * @since 09 May 2016
- */
-export default (element, name) => element.dataset[name] || null;
